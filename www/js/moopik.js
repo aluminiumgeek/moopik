9b0bfb91b545393185d33a8261ddda38b011ff5a
aluminiumgeek <mihail265@gmail.com>
2014-05-21 12:39:25 +0400

    web sql

--- www/js/moopik.js
+++ www/js/moopik.js
@@ -1,16 +1,49 @@
-var moopik = (function() {
+var moopik = (function($) {
   var self = {};
   
   self.init = function() {
+    self.db = window.openDatabase("moopik", "1.0", "Moopik", 1000000);
+    db.transaction(populateDb, dbErr);
+    
+    db.transaction(getLocations, dbErr);
+    
     navigator.geolocation.getCurrentPosition(geoOnSuccess, geoOnError);
   }
   
   function geoOnSuccess(position) {
-    log(JSON.stringify(position));
+    var location = JSON.stringify(position);
+    log(location);
+    
+    self.position = position;
+    
+    db.transaction(function(tx) {
+      tx.executeSql("INSERT INTO locations(location) VALUES ("+location+")");
+    });
   };
   function geoOnError() {
     log('Error while geolocating');
   }
   
+  function populateDb(tx) {
+    tx.ececuteSql("CREATE TABLE IF NOT EXISTS locations(id INTEGER PRIMARY KEY ASC, location TEXT NOT NULL)");
+  }
+  
+  function getLocations(tx) {
+    tx.executeSql(
+      "SELECT location from locations ORDER BY id DESC LIMIT 5",
+      [],
+      renderLocations,
+      dbErr
+    );
+  };
+  
+  function renderLocations(tx, items) {
+    log(JSON.stringify(items));
+  }
+  
+  function dbErr(err) {
+    log(JSON.stringify(err));
+  }
+  
   return self;
-})();
+})(jQuery);
