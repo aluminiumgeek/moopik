63ac82c44f751beff98eb3b3983a454ab539afe5
aluminiumgeek <mihail265@gmail.com>
2014-05-21 12:05:43 +0400

    jquery mobile

--- www/js/moopik.js
+++ www/js/moopik.js
@@ -2,12 +2,10 @@ var moopik = (function() {
   var self = {};
   
   self.init = function() {
-    log('Getting position');
     navigator.geolocation.getCurrentPosition(geoOnSuccess, geoOnError);
   }
   
   function geoOnSuccess(position) {
-    log('Success');
     log(JSON.stringify(position));
   };
   function geoOnError() {
