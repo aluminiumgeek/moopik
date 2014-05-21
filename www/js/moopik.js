819d7e917cd068dfcb3422fbf5f3f2d97e750698
aluminiumgeek <mihail265@gmail.com>
2014-05-21 10:48:45 +0400

    log geolocation

--- www/js/moopik.js
+++ www/js/moopik.js
@@ -2,6 +2,7 @@ var moopik = (function() {
   var self = {};
   
   self.init() = function() {
+    log('Getting position');
     navigator.geolocation.getCurrentPosition(
       geoOnSuccess, 
       geoOnError, 
